e3fac29b70c4ce595f5f4b5e9802e8533d34d1c5
maziarrezaei <maziar.rezaei@gmail.com>
2019-10-07 19:47:59 -0400

    Fixed nuget issues. Tested for ProjectReference mode. Initial beta version marked.

---
+++ MakeNugetPackage.ps1
@@ -0,0 +1,54 @@
+cd $PSScriptRoot
+
+$t4FilePath = '.\ProductivityExtensionMethods\ProductivityExtension.methods.tt';
+
+if (-not (Test-Path $t4FilePath -PathType Leaf))
+{
+    Write-Error 'Cannot find ProductivityExtension.methods.tt. Open the solution in visual studio and save the t4 template file to generate this file.'
+    exit 1;
+}
+
+try
+{
+    $node = Select-Xml -Path .\ProductivityExtensionMethods.nuspec -XPath '/ns:package/ns:metadata/ns:version' -Namespace @{ns = 'http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd' }
+    $version = $node.Node.InnerXML;
+
+    if ([String]::IsNullOrWhiteSpace($version))
+    {
+        throw "";
+    }
+}
+catch
+{
+    Write-Error 'Unable to read the version from ProductivityExtensionMethods.nuspec. Ensure version is there!'
+    exit 1;
+}
+
+$input = Read-Host "Create nuget package with version ($version)? (y/n)"
+
+if ($input.ToLower() -ne 'y')
+{
+    exit 0;
+}
+
+$fileContent = Get-Content $t4FilePath -Raw
+
+$fileContent = $fileContent -replace 'VersionPlaceholder{D8B1B561-500C-4086-91AA-0714457205DA}', $version
+
+Set-Content $t4FilePath -Value $fileContent
+
+if (Test-Path './nuget.exe')
+{
+    ./nuget pack ProductivityExtensionMethods.nuspec
+}
+elseif (Get-Command nuget -ErrorAction SilentlyContinue)
+{
+    nuget pack ProductivityExtensionMethods.nuspec
+}
+else
+{
+    Write-Error 'nuget.exe not found. Please either copy next to the script, or add to the Path environment variable.'
+    exit 1;
+}
+
+
---
+++ init.ps1
@@ -0,0 +1,99 @@
+﻿using namespace System.IO
+param($installPath, $toolsPath, $package, $project)
+
+function ParseT4Content([String] $content)
+{
+    $regEx = [regex] '//ToolsVersion:(?<Version>(?:\d+\.){2}\d+)';
+
+    $match = $regEx.Match($content);
+
+    if (-not $match.Success)
+    {
+        return $null;
+    }
+
+    return @{
+        Version       = $match.Groups['Version'].Value
+        ConfigSection = $content.Substring(0, $match.Index)
+        CodeSection   = $content.Substring($match.Index)
+    };
+}
+
+
+
+function ApplyOldConfigToNew([String] $oldConfig, [String] $newConfig)
+{
+    # Reads the config values from the old config, and applies them to the new config
+    # Returns the new updated config created.
+
+    $variableRegex = [regex] '(?<=\s(?<Name>[a-zA-Z0-9_]+)\s*=\s*)(?<Value>true|false)\s*(?=;)';
+
+    $oldConfigValues = $variableRegex.Matches($oldConfig) | % { [PSCustomObject]@{ 
+                                                                    Name  = $_.Groups['Name'].Value;
+                                                                    Value = $_.Groups['Value'].Value
+                                                                }
+    };
+
+    return $variableRegex.Replace($newConfig,
+        {
+            $newConfigMatch = $args[0];
+
+            $oldConfig = $oldConfigValues | where { $_.Name -eq $newConfigMatch.Groups['Name'].Value } | select -First 1;
+
+            if ($oldConfig)
+            {
+                return $oldConfig.Value;
+            }
+            else
+            {
+                #do not replace,
+                return $newConfigMatch.Groups['Value'].Value;
+            }
+        });
+}
+
+function EnsureFileAddedToProject([String] $filePath, $project)
+{
+    $projItem = $project.ProjectItems | where { $_.Properties | where { $_.Value -eq $filePath } };
+
+    if (-not $projItem)
+    {
+        $projItem = $project.ProjectItems.AddFromFile($filePath);
+    }
+
+
+    $customToolsProp = $projItem.Properties | where { $_.Name -eq "CustomTool" } | select -First 1
+
+    if($customToolsProp.Value -ne 'TextTemplatingFileGenerator')
+    {
+        $customToolsProp.Value='TextTemplatingFileGenerator'
+    }
+}
+
+$toolsDir=[Path]::GetDirectoryName($toolsPath);
+
+$packageT4Path = "$toolsDir\..\assets\ProductivityExtension.methods.tt";
+
+$t4Path = [Path]::Combine([Path]::GetDirectoryName($project.Properties.Item('FullPath').Value), "ProductivityExtensions.tt");
+
+if (Test-Path $t4Path -PathType Leaf)
+{
+    #read the file, and only overwrite the code section
+    $currentT4File = ParseT4Content (Get-Content $t4Path -Raw)
+    $packageT4File = ParseT4Content (Get-Content $packageT4Path -ra);
+
+    
+    if ($currentT4File.Version -ne $packageT4File.Version)
+    {
+        $currentT4File.ConfigSection = ApplyOldConfigToNew -oldConfig $currentT4File.ConfigSection -newConfig $packageT4File.ConfigSection
+    }
+
+    Set-Content $t4Path -Value ($currentT4File.ConfigSection + $packageT4File.CodeSection)
+}
+else
+{
+    #copy the file to the project path. Include in project file if necessary.
+    Copy-Item $packageT4Path $t4Path
+
+    EnsureFileAddedToProject $t4Path $project
+}
