9e8ce3fd939c1851507291fc3adbb76d72e7f06f
maziarrezaei <maziar.rezaei@gmail.com>
2019-12-05 00:24:59 -0500

    Fixed the issue of Roslyn analyzer not covering code that was disabled by #if blocks.
    This caused the null reference types in one of the #if or #else block not being marked.

--- CodePreprocessor/Program.cs
+++ CodePreprocessor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace CodePreprocessor
 {
@@ -7,24 +8,30 @@ namespace CodePreprocessor
     {
         static int Main(string[] args)
         {
-            if(args.Length==0)
+            const string uniqueComment = "//asdfjwieowilsdkvmmweiowjaX";
+
+            if (args.Length == 0)
             {
                 Console.Error.Write("Tool must be supplied with the path to a .cs code file.");
                 return 1;
             }
             string path = args[0];
 
-            if(!File.Exists(path))
+            if (!File.Exists(path))
             {
-                Console.Error.Write("File not found: {0}",path);
+                Console.Error.Write("File not found: {0}", path);
                 return 2;
             }
 
             string code = File.ReadAllText(path);
 
+            var ifPreprocessorRegX = new Regex("^#(if|elif|endif|else)",RegexOptions.Multiline|RegexOptions.ExplicitCapture);
+
+            code = ifPreprocessorRegX.Replace(code, uniqueComment + @"$0");
+
             string processedCode = AnalyzerRunner.RunCodeFix("C#", new NullableReferenceTypeAnalyzer(), new NullableReferenceTypeCodeFixProvider(), code, true);
 
-            Console.Write(processedCode);
+            Console.Write(processedCode.Replace(uniqueComment, ""));
 
             return 0;
         }
